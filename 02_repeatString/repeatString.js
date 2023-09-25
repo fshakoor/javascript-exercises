f31f00b85eb31ca9028061efb4733eb6f8ee92d0
Faiz Shakoor <fshakoortx@utexas.edu>
2023-09-25 16:23:51 -0500

    Fixed function repeatString to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str,int) {
+    if (int < 0) {return 'ERROR'};
+    let newstr = ''
+    for (let i = 0; i < int; i++) {
+        newstr += str
+    };
+    return newstr
 };
 
 // Do not edit below this line
