db615646d29afcd2f9148dd1f3ff14304319c7de
Faiz Shakoor <fshakoortx@utexas.edu>
2023-09-25 18:23:25 -0500

    Fixed sumAll function to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(int1,int2) {
+    if (int1 < 0 || int2 < 0) {return 'ERROR'}
+    if (typeof(int1) != "number" || typeof(int2) != "number") {return 'ERROR'}
+    sum = 0;
+    if (int1 < int2) {
+        for (i = int1; i < int2+1; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = int1; i > int2-1; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
