77823264115252aa558bc46c696b81f16df0b2e8
Faiz Shakoor <fshakoortx@utexas.edu>
2023-09-25 18:45:00 -0500

    fixed leapYears function to pass tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let flag = false;
+    if (year % 4 == 0) {flag = true}
+    if (year % 100 == 0 && year % 400 !== 0) {flag = false}
+    return flag
 };
 
 // Do not edit below this line
