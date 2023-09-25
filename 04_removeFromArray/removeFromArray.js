1275fa095d56ff94fde4d3e6c79d8fd773a8b0a3
Faiz Shakoor <fshakoortx@utexas.edu>
2023-09-25 18:07:46 -0500

    fixed removefromarray to pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...toremove) {
+    let args = [...toremove]
+    for (const i of args) {
+        let index = arr.indexOf(i);
+        if (index >= 0) {arr.splice(index,1)}
+    }
+    return arr
 };
 
 // Do not edit below this line
