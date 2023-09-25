8fe548710658538b5b213ea9414d7f4cecfb2072
Faiz Shakoor <fshakoortx@utexas.edu>
2023-09-25 16:33:49 -0500

    Fixed function reverseString to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = []
+    for (const letters of str) {
+        arr.push(letters);
+    }
+    arr = arr.reverse()
+    return arr.join('')
 };
 
 // Do not edit below this line
