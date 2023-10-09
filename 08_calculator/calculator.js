db3af699b6b47a40c700c0dd0645ef938a0d32ff
Faiz Shakoor <fshakoortx@utexas.edu>
2023-10-08 20:45:27 -0500

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...args) {
+  return args.reduce((sum, current) => sum + current);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((sum, current) => sum - current);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  arr.forEach(element => {
+    result += element
+  });
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  arr.forEach(element => {
+    result *= element
+  });
+  return result
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  while (num > 0) {
+    result *= num
+    num -= 1
+  }
+  return result
 };
 
 // Do not edit below this line
