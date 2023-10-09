8a989d54429e5ead953011bc9666a81128028035
Faiz Shakoor <fshakoortx@utexas.edu>
2023-10-08 21:38:27 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if (n<0) {return "OOPS"}
+    let fib = [1, 1, 2, 3, 5, 8]
+    if (fib[n+1] == undefined) {
+      while (fib.length < n) {
+        fib.push(fib[fib.length-1]+fib[fib.length-2])
+      }
+    }
+    return fib[n-1]
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
