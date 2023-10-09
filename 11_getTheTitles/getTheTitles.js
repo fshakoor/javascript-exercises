0c10641efab6b8b4aba1123c55e2df6090caee1b
Faiz Shakoor <fshakoortx@utexas.edu>
2023-10-08 21:42:35 -0500

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let result = [];
+      books.forEach(element => {
+        result.push(element.title)
+      });
+    return result;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
