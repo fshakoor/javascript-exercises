44c7b7324a40615fbba4610d6431a03db2a5dcb8
Faiz Shakoor <fshakoortx@utexas.edu>
2023-10-08 21:09:01 -0500

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    str = str.toLowerCase()
+      let strArr = str.split('')
+      while (strArr.includes('!')) {
+        strArr.splice(strArr.indexOf('!'),1);
+      }
+      while (strArr.includes('.')) {
+        strArr.splice(strArr.indexOf('.'),1);
+      }
+      while (strArr.includes(',')) {
+        strArr.splice(strArr.indexOf(','),1);
+      }
+      while (strArr.includes(' ')) {
+        strArr.splice(strArr.indexOf(' '),1);
+      }
+      str = strArr.join('')
+      let reverse = str.split('').reverse().join('')
+      return str === reverse ? true : false
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
