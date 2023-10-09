37b81a3f10c56eab5b3bc510cf49af8ecc9f30a3
Faiz Shakoor <fshakoortx@utexas.edu>
2023-10-08 21:53:41 -0500

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let result = '';
+    people.forEach(person => {
+      if (person.yearOfDeath == undefined) {person.yearOfDeath = 2023}
+      let age = person.yearOfDeath - person.yearOfBirth;
+      if ((age) > oldest) {
+        oldest = age;
+        result = person;
+      }
+    });
+    return result;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
